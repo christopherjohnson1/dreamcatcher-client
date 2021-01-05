e2ec538ea9431c1084df7142845629b7772db20a
Chris Johnson <christopherjohnson1@gmail.com>
2021-01-05 12:55:45 -0500

    iwp

--- src/components/dreams/AllDreams.js
+++ src/components/dreams/AllDreams.js
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { DreamsContext } from './DreamsProvider'
 import { ProfileContext } from '../auth/AuthProvider'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import './AllDreams.css'
 
 export const AllDreams = props => {
@@ -9,12 +13,16 @@ export const AllDreams = props => {
 
     const [ showOthersDreams, setShowOthersDreams ] = useState(true)
 
+    const [view, setView] = useState('all')
+
+
     // initialization effect hook to get dreams
     useEffect(() => {
         if (props.match) {
             if (props.match.path === '/all-dreams/my-dreams') {
                 if (profile.user) {
                     getDreamsByUser(profile.user.id)
+                    setView('mydreams')
                     setShowOthersDreams(false)
                 }
             }
@@ -23,12 +31,44 @@ export const AllDreams = props => {
             getAllDreams()
             setShowOthersDreams(true)
         }
-    }, [])
+    }, [profile, props.match])
     
-    showOthersDreams ? console.log('dreams', dreams) : console.log('myDreams', myDreams)
 
     return (
     <>
+        <div className="all-dreams container">
+            <h1 className="text-center">
+                {view === 'mydreams' ? 'My' : 'All'} Dreams
+            </h1>
+
+            {showOthersDreams ?
+                dreams.map(d => {
+                    return <Card body className="my-3 d-flex">
+                            <Container>
+                                <Row>
+                                    <Col>{d.date}</Col>
+                                    <Col className="text-center">{d.title}</Col>
+                                    <Col className="text-center">{d.user.full_name}</Col>
+                                </Row>
+                                <Row>
+                                    <Col className="text-center pt-4">{d.dream_type.label}</Col>
+                                </Row>
+                            </Container>
+                        </Card>
+                }) 
+                : myDreams.map(d => {
+                    return <Card body className="my-3 d-flex">
+                    <Container>
+                        <Row>
+                            <Col>{d.date}</Col>
+                            <Col className="text-center">{d.title}</Col>
+                            <Col className="text-center">{profile.full_name}</Col>
+                        </Row>
+                    </Container>
+                </Card>
+                })}
+
+        </div>
     </>
     )
 }
